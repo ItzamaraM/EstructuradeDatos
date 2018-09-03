2f496205ddcf42308bc56d9bac807435a2bb3291
ItzamaraM <32503853+ItzamaraM@users.noreply.github.com>
2018-09-02 23:01:31 -0500

    Create TDA.java

---
+++ TDA.java
@@ -0,0 +1,22 @@
+package estructuradedatos.diagnostica;
+
+
+public class TDA {
+    
+    public static void main(String[]args){
+        int edad=50;//primitivo
+        Estudiante x;//Dato abstracto, x=intancia de clase
+        System.out.println(edad); //unsigned int son los numeros naturales
+        System.out.println(Byte.MIN_VALUE+".."+Byte.MAX_VALUE); //Clase envonvente es la definicion de un 
+        //tipo de dato abstracto que se refiere a un preconstruido
+        System.out.println(Float.);
+    }
+    
+}
+
+//Definicion del dato abstracto
+//la palabra Class define un tipo de variable como el "int"
+class Estudiante{
+    private String nControl, nombre, email;
+    private int edad;
+}
