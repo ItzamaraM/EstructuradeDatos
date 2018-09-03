0c51010d1c05b91109ac12ce076485cc93569927
ItzamaraM <32503853+ItzamaraM@users.noreply.github.com>
2018-09-02 23:00:29 -0500

    Create ParaCharlie.java

---
+++ ParaCharlie.java
@@ -0,0 +1,13 @@
+package estructuradedatos.diagnostica;
+
+import javax.swing.JOptionPane;
+
+public class ParaCharlie {
+public static void main(String[]args){
+    new javax.swing.JOptionPane().showMessageDialog(null,"Charlie",null,-1
+    JOptionPane.PLAIN_MESSAGE);
+
+}
+
+    
+}
